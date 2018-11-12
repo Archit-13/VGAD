141c55f7e8773418e1cf1e2ec6f22d528a4ebdf1
Archit-13 <43059219+Archit-13@users.noreply.github.com>
2018-11-11 23:35:49 -0700

    hope this works

---
+++ KinematicBody2D.gd
@@ -0,0 +1,48 @@
+extends KinematicBody2D
+
+const UP =Vector2(0,-1)
+const Gravity=20
+const Jump=-1100
+const Acceleration=100
+const MaxSpeed=400
+var jump = Jump -10000
+var motion=Vector2()
+var friction=false
+func _physics_process(delta):
+	motion.y+=Gravity
+	if Input.is_action_pressed("ui_right"):
+		$Brassman.flip_h=false
+		$Brassman/Trambone.flip_h=false
+		$Brassman.play("Run")
+		motion.x=min(motion.x+Acceleration,MaxSpeed)
+		if Input.is_action_just_pressed("ui_accept"):
+			self.position.x+=50
+	elif Input.is_action_pressed("ui_left"):
+		$Brassman.flip_h=true
+		$Brassman/Trambone.flip_h=true
+		$Brassman.play("Run")
+		motion.x=max(motion.x-Acceleration,-MaxSpeed)
+		if Input.is_action_just_pressed("ui_accept"):
+			self.position.x-=50
+	else:
+		$Brassman.play("idle")
+		motion.x=0
+	if is_on_floor():
+		jump = Jump - 10000
+		if Input.is_action_pressed("ui_up"):
+			motion.y=Jump
+		if friction==true:
+			motion.x=lerp(motion.x,0,0)
+	else:
+		$Brassman.play("Jump")
+		if friction==false:
+			motion.x=lerp(motion.x,0,0)
+	if is_on_wall():
+		if Input.is_action_pressed("ui_accept"):
+			motion.y=0
+			if Input.is_action_pressed("ui_up"):
+				motion.y=jump
+				jump = 0
+				
+			
+	motion=move_and_slide(motion,UP)
